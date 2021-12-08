097121e439ee643d874a755eb3d6ccd081d4b83c
William Noris <w.noris@protonmail.com>
2021-12-08 09:36:28 +0100

    initial commit

---
+++ src/main/java/fr/william/heritage/HeritageApplication.java
@@ -0,0 +1,13 @@
+package fr.william.heritage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeritageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeritageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/william/heritage/HeritageApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.william.heritage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeritageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
